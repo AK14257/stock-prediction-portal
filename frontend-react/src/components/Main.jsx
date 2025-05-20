85e2e1bb6ca93e7cf9979ba5519dc44c0bfffd0f
Abhishek Kumar Yadav <abhishekyadv347754@gmail.com>
2025-05-21 00:55:44 +0530

    98

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+    <div className='container'>
+      <div className='p-5 text-center bg-light-dark'>
+        <h1 className='text-light'>Stock Prediction Portal</h1>
+        <p className='text-light lead'>A stock prediction system uses historical market data, 
+          machine learning algorithms, and statistical models to forecast future stock prices.
+           It analyzes trends, patterns, and financial indicators to help investors 
+           make informed decisions. While predictions aren't guaranteed, 
+           such systems enhance decision-making in the volatile and complex world of stock trading.
+           </p>
+           <Button text='Login' class='btn-outline-warning'/>
+      </div>
+      
+
+
+    </div>
+    </>
+  )
+}
+
+export default Main
